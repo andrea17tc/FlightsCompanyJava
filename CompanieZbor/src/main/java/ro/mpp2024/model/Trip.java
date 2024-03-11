6bb21a8f46a910a2ca5a895fb2da15eb821b4656
andrea17tc <andreatcaciuc7@gmail.com>
2024-03-11 12:41:22 +0200

    trip mod

--- CompanieZbor/src/main/java/ro/mpp2024/model/Trip.java
+++ CompanieZbor/src/main/java/ro/mpp2024/model/Trip.java
@@ -1,6 +1,52 @@
 package ro.mpp2024.model;
 
+import java.util.Objects;
+
 public class Trip {
-    private int flightID;
+    private int touristID;
     private int purchaseID;
+
+
+    public Trip(int touristID, int purchaseID) {
+        this.touristID = touristID;
+        this.purchaseID = purchaseID;
+    }
+
+
+    public int getTouristID() {
+        return touristID;
+    }
+
+    public void setTouristID(int touristID) {
+        this.touristID = touristID;
+    }
+
+    public int getPurchaseID() {
+        return purchaseID;
+    }
+
+    public void setPurchaseID(int purchaseID) {
+        this.purchaseID = purchaseID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Trip trip = (Trip) o;
+        return touristID == trip.touristID && purchaseID == trip.purchaseID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(touristID, purchaseID);
+    }
+
+    @Override
+    public String toString() {
+        return "Trip{" +
+                "touristID=" + touristID +
+                ", purchaseID=" + purchaseID +
+                '}';
+    }
 }
